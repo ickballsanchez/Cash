2791b0d4d728ba31cf63debc0394124f90e4d52b
P4 <P4@TK_014>
2021-10-27 14:31:54 -0400

    Add project files.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.slicesInput = new System.Windows.Forms.TextBox();
+            this.breadInput = new System.Windows.Forms.TextBox();
+            this.drinkInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.receiptLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Slices";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Bread";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Drank";
+            // 
+            // slicesInput
+            // 
+            this.slicesInput.Location = new System.Drawing.Point(106, 12);
+            this.slicesInput.Name = "slicesInput";
+            this.slicesInput.Size = new System.Drawing.Size(100, 20);
+            this.slicesInput.TabIndex = 3;
+            // 
+            // breadInput
+            // 
+            this.breadInput.Location = new System.Drawing.Point(106, 37);
+            this.breadInput.Name = "breadInput";
+            this.breadInput.Size = new System.Drawing.Size(100, 20);
+            this.breadInput.TabIndex = 4;
+            // 
+            // drinkInput
+            // 
+            this.drinkInput.Location = new System.Drawing.Point(106, 63);
+            this.drinkInput.Name = "drinkInput";
+            this.drinkInput.Size = new System.Drawing.Size(100, 20);
+            this.drinkInput.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Location = new System.Drawing.Point(2, 151);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(46, 13);
+            this.subtotalLabel.TabIndex = 7;
+            this.subtotalLabel.Text = "Subtotal";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(2, 177);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(25, 13);
+            this.taxLabel.TabIndex = 8;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(2, 207);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 9;
+            this.totalLabel.Text = "Total";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(319, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 10;
+            // 
+            // receiptLabel
+            // 
+            this.receiptLabel.BackColor = System.Drawing.Color.White;
+            this.receiptLabel.Location = new System.Drawing.Point(240, 24);
+            this.receiptLabel.Name = "receiptLabel";
+            this.receiptLabel.Size = new System.Drawing.Size(173, 244);
+            this.receiptLabel.TabIndex = 14;
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Location = new System.Drawing.Point(31, 244);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(75, 23);
+            this.receiptButton.TabIndex = 15;
+            this.receiptButton.Text = "Print";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.receiptLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.drinkInput);
+            this.Controls.Add(this.breadInput);
+            this.Controls.Add(this.slicesInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox slicesInput;
+        private System.Windows.Forms.TextBox breadInput;
+        private System.Windows.Forms.TextBox drinkInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label receiptLabel;
+        private System.Windows.Forms.Button receiptButton;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+
+        //variables here
+        double slicePrice = 2.25;
+        double breadPrice = 2;
+        double drinkPrice = 1.50;
+
+        int numbOfSlice = 0;
+        int numbOfBread = 0;
+        int numbOfDrink = 0;
+
+        double subtotal;
+        double taxPercent = 0.13;
+        double taxValue;
+        double total;
+
+        //subtotal, taxRate, tasAmount, total
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // get the inputs and put them in the variables
+            numbOfSlice = Convert.ToInt32(slicesInput.Text);
+            numbOfBread = Convert.ToInt32(breadInput.Text);
+            numbOfDrink = Convert.ToInt32(drinkInput.Text);
+
+            subtotal = numbOfSlice * slicePrice + numbOfBread * breadPrice + numbOfDrink * drinkPrice;
+            subtotalLabel.Text = subtotal.ToString();
+            taxValue = subtotal * taxPercent;
+            total = subtotal + taxValue;
+            taxLabel.Text = taxValue.ToString(); 
+            totalLabel.Text = total.ToString();
+
+            
+            
+            
+            
+
+
+
+            
+            
+
+
+
+
+
+
+            
+
+            //calc your subtotal
+
+            //calc your tax
+
+            //calc your total
+
+            //output all the totals to labels
+        }
+
+        private void printerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            receiptLabel.Text = "Coles Pizza Place";
+            receiptLabel.Refresh();
+            Thread.Sleep(300);
+
+            receiptLabel.Text += "\n\nOctober 26,2021";
+            receiptLabel.Refresh();
+            Thread.Sleep(300);
+
+            receiptLabel.Text += $"\n\nSlices x {numbOfSlice}   @ {slicePrice} ";
+            receiptLabel.Refresh();
+            Thread.Sleep(300);
+
+            receiptLabel.Text += $"\n\nBread x {numbOfBread}   @ {breadPrice} ";
+            receiptLabel.Refresh();
+            Thread.Sleep(300);
+
+            receiptLabel.Text += $"\n\nDrank x {numbOfDrink}   @ {drinkPrice} ";
+            receiptLabel.Refresh();
+            Thread.Sleep(300);
+
+            receiptLabel.Text += $"\n\nSubtotal {subtotal}";
+            receiptLabel.Refresh();
+            Thread.Sleep(300);
+
+            receiptLabel.Text += $"\n\nTax {taxValue}";
+            receiptLabel.Refresh();
+            Thread.Sleep(300);
+
+            receiptLabel.Text += $"\n\nTotal {total}";
+            receiptLabel.Refresh();
+            Thread.Sleep(300);
+
+            receiptLabel.Text += $"\n\n Have a Great Day!";
+            
+           
+
+
+
+        }
+    }
+}
